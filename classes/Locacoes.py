f5a0ced0311efa554e26165f13f7a2d81b766ccc
Pedro Figueiredo <pedrofernando2003@gmail.com>
2024-03-19 17:12:44 -0400

    Iniciando Locacoes.py

---
+++ classes/Locacoes.py
@@ -0,0 +1,12 @@
+class Locacoes:
+    __id_locacao = None
+    __carro = None
+    __locatario = None
+    __data_locacao = None
+    __data_devolucao = None
+    __km_inicial = None
+    __km_final = None
+    __seguro = None
+    __valor_total = None
+
+    def __init__(self, id_locacao, id_carro, )
